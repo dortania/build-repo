4d145d331f9e570fa1f929021f3d5a2e100b8637
Dhinak G <dhinak2004@gmail.com>
2020-05-13 14:51:19 -0400

    oops

--- parallel_check.py
+++ parallel_check.py
@@ -13,10 +13,10 @@ workflow_url = this_run.GET().json()["workflow_url"]
 #workflow_output = hammock(workflow_url).GET(auth=("dhinakg", token))
 #workflow_id = json.loads(workflow_output.text or workflow_url.content)["id"]
 
-runs = workflow_url.runs.GET().json()
+runs = hammock(workflow_url).runs.GET().json()
 for run in runs["workflow_runs"]:
     if run["id"] != os.environ["GITHUB_RUN_ID"] and run["status"] != "completed":
-        print("Another build is running, cancelling this one...")
+        print("Another build iss running, cancelling this one...")
         cancel_request = this_run.cancel.POST()
         if cancel_request.status_code != 202:
             sys.exit("Status code did not match: " + cancel_request.status_code)
