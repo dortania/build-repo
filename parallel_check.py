9b2126db60cc0ae6ca188ace1b1c8c52790b2233
Dhinak G <dhinak2004@gmail.com>
2020-05-13 13:52:16 -0400

    Create parallel_check.py

---
+++ parallel_check.py
@@ -0,0 +1,10 @@
+from hammock import Hammock as hammock
+import json
+import sys
+
+with open("gh token.txt") as f:
+    token = f.read().strip()
+eee = hammock("https://api.github.com/repos/dhinakg/ktextrepo/actions/workflows/1273918/runs").GET(auth=("dhinakg",token))
+output = json.loads(eee.text or eee.content)
+if output["workflow_runs"][1]["status"] != "completed":
+    sys.exit("Another build is already running!")
