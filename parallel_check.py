954d98c4d177308a35c148b1d65784196e601246
Dhinak G <dhinak2004@gmail.com>
2020-05-13 14:45:35 -0400

    Updated parallel check

--- parallel_check.py
+++ parallel_check.py
@@ -1,11 +1,25 @@
-from hammock import Hammock as hammock
 import json
 import sys
+import os
+from hammock import Hammock as hammock
 
 with open("gh token.txt") as f:
     token = f.read().strip()
-eee = hammock("https://api.github.com/repos/dhinakg/ktextrepo/actions/workflows/1273918/runs").GET(auth=("dhinakg",token))
-output = json.loads(eee.text or eee.content)
-for z in output["workflow_runs"]:
-    if z["status"] != "completed":
-        sys.exit("Another build is already running!")
+
+
+this_run = hammock(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/actions/runs/{os.environ['GITHUB_RUN_ID']}", auth=("dhinakg", token))
+
+workflow_url = this_run.GET().json()["workflow_url"]
+
+#workflow_output = hammock(workflow_url).GET(auth=("dhinakg", token))
+#workflow_id = json.loads(workflow_output.text or workflow_url.content)["id"]
+
+runs = this_run.runs.GET().json()
+for run in runs["workflow_runs"]:
+    if run["id"] != os.environ["GITHUB_RUN_ID"] and run["status"] != "completed":
+        print("Another build is running, cancelling this one...")
+        cancel_request = this_run.cancel.POST()
+        if cancel_request.status_code != 202:
+            sys.exit("Status code did not match: " + cancel_request.status_code)
+        else:
+            sys.exit(0)
