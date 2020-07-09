8d487286552896e7aa78395ad2a2f72189eff463
Dhinak G <dhinak2004@gmail.com>
2020-07-08 22:14:35 -0400

    Add ensure latest to downloader

--- downloader.py
+++ downloader.py
@@ -4,16 +4,40 @@ import zipfile
 from pathlib import Path
 from hammock import Hammock as hammock
 
-plugins = hammock("https://raw.githubusercontent.com/dhinakg/ktextrepo/builds/config.json").GET()
+plugins = hammock("https://raw.githubusercontent.com/dhinakg/ktextrepo/github-actions/plugins.json").GET()
 plugins = json.loads(plugins.text)
+
+config = hammock("https://raw.githubusercontent.com/dhinakg/ktextrepo/builds/config.json").GET()
+config = json.loads(config.text)
 print("Global Settings: ")
+ensure_latest = bool(distutils.util.strtobool(input("Ensure latest? (\"true\" or \"false\") ").lower()))
 unzip = bool(distutils.util.strtobool(input("Unzip automatically and delete zip? (\"true\" or \"false\") ").lower()))
 extract_dir = input("Put files in directory (leave blank for current dir): ") if unzip else None
 while True:
     target = input("Enter product to download (case sensitive): ")
     dbg = input("Debug or release? (\"debug\" or \"release\") ").lower()
     try:
-        to_dl = plugins[target]["versions"][0]
+        if ensure_latest:
+            organization = repo = None
+            for plugin in plugins["Plugins"]:
+                if plugin["Name"] == target:
+                    organization, repo = plugin["URL"].strip().replace("https://github.com/", "").split("/")
+                    break
+            if not repo:
+                print("Product " + target + " not available\n")
+                continue
+            commits_url = hammock("https://api.github.com").repos(organization, repo).commits.GET(params={"per_page": 100})
+            commit_hash = json.loads(commits_url.text or commits_url.content)[0]["sha"]
+            to_dl = None
+            for i in config[target]["versions"]:
+                if i["commit"] == commit_hash:
+                    to_dl = i
+                    break
+            if not to_dl:
+                print("Latest version (" + commit_hash + ") unavailable\n")
+                continue
+        else:
+            to_dl = config[target]["versions"][0]
         dl_link = to_dl["links"][dbg]
         print(f"Downloading {target} version {to_dl['version']} sha {to_dl['commit']} and date added {to_dl['dateadded']}")
     except KeyError as error:
