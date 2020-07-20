9cbdbbc67029ae998727516a3c8d292ee9be31c4
Dhinak G <dhinak2004@gmail.com>
2020-07-19 23:09:59 -0400

    sync

--- test_release.py
+++ test_release.py
@@ -20,24 +20,30 @@ get_release = url_string.GET()
 for i in [i["id"] for i in get_release.json()]:
     af= url_string(i).DELETE()
 
+delete_tags = hammock(f"https://api.github.com/repos/dhinakg/ktextrepo-beta/git/matching-refs/tags", auth=("dhinakg", token))
 
-derp = hammock("https://api.github.com/repos/dhinakg/ktextrepo-beta/git/refs/tags/testtag", auth=("dhinakg", token)).DELETE()
+delete_tags = delete_tags.GET()
+for i in [i["url"] for i in delete_tags.json()]:
+    derp = hammock(i, auth=("dhinakg", token)).DELETE()
 
 
-create_release = url_string.POST(json={
-    "tag_name": "bb5e7f8322d6c73b1888f2ecb20a5eebef63e90a",
-    "target_commitish": "builds",
-    "name": "AirportBrcmFixup-bb5e7f8"
-})
+# derp = hammock("https://api.github.com/repos/dhinakg/ktextrepo-beta/git/refs/tags/testtag", auth=("dhinakg", token)).DELETE()
 
-create_release_json = create_release.json()
 
-pp.pprint(create_release_json["id"])
+# create_release = url_string.POST(json={
+#     "tag_name": "bb5e7f8322d6c73b1888f2ecb20a5eebef63e90a",
+#     "target_commitish": "builds",
+#     "name": "AirportBrcmFixup-bb5e7f8"
+# })
 
-add_release_asset = hammock(str(purl.Template(create_release_json["upload_url"]).expand({"name": "derp", "label": "YEEEE"})), auth=("dhinakg", token)).POST(data="hehe", headers={"content-type": "text/plain; charset=us-ascii"})
-print(add_release_asset)
-if str(add_release_asset.json()) != "":
-    pp.pprint(add_release_asset.json())
+# create_release_json = create_release.json()
+
+# pp.pprint(create_release_json["id"])
+
+# add_release_asset = hammock(str(purl.Template(create_release_json["upload_url"]).expand({"name": "derp", "label": "YEEEE"})), auth=("dhinakg", token)).POST(data="hehe", headers={"content-type": "text/plain; charset=us-ascii"})
+# print(add_release_asset)
+# if str(add_release_asset.json()) != "":
+#     pp.pprint(add_release_asset.json())
 
 
 # 28716357
