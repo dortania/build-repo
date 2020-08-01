f93706759bdac9ac5b4182d5272afbf5121e9092
Dhinak G <dhinak2004@gmail.com>
2020-07-31 23:49:27 -0400

    typo

--- update_config.py
+++ update_config.py
@@ -87,7 +87,7 @@ for i in config:
         if rel.get("html_url"):
             config[i]["versions"][j]["release"]["url"] = rel["html_url"]
     for j, item in [(j, item) for (j, item) in enumerate(config[i]["versions"]) if item.get("release", {}).get("description", None)]:
-        config[i]["versions"][j]["release"]["description"] = config[i]["versions"][j]["release"]["description"].replace("**Hashes**:\n\nDebug:\n\n", "**Hashes**:\n**Debug:**\n").replace("\nRelease:\n\n", "**Release:**\n").replace("\nExtras:\n\n", "**Extras:**\n").replace(config[i]["versions"][j]["commit"]["sha"], "View on GitHub").replace("**Changes:**\n**Changes:**\n", "**Changes:**\n")
+        config[i]["versions"][j]["release"]["description"] = config[i]["versions"][j]["release"]["description"].replace("**Hashes**:\n\nDebug:\n\n", "**Hashes**:\n**Debug:**\n").replace("\nRelease:\n\n", "**Release:**\n").replace("\nExtras:\n\n", "**Extras:**\n").replace(config[i]["versions"][j]["commit"]["sha"] + "]", "View on GitHub").replace("**Changes:**\n**Changes:**\n", "**Changes:**\n").replace("View on GitHub)", config[i]["versions"][j]["commit"]["sha"] + ")")
         if config[i]["versions"][j]["release"]["description"][:13] != "**Changes:**\n":
             config[i]["versions"][j]["release"]["description"] = "**Changes:**\n" + config[i]["versions"][j]["release"]["description"]
     config[i]["versions"] = [i for i in config[i]["versions"] if i.get("release", {}).get("url", True)]
