c30f89e268592c8605c4c899c65c20fea242b8ba
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2022-07-17 12:38:54 -0400

    Satisfy linter

--- parallel_check.py
+++ parallel_check.py
@@ -1,29 +1,31 @@
-import json
 import os
 import sys
 import time
-from hammock import Hammock as hammock
+
+import requests
 
 token = sys.argv[1].strip()
 
+session = requests.Session()
+session.auth = ("github-actions", token)
 
-this_run = hammock(f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/actions/runs/{os.environ['GITHUB_RUN_ID']}", auth=("github-actions", token))
-workflow_url = this_run.GET().json()["workflow_url"]
+this_run_url = f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/actions/runs/{os.environ['GITHUB_RUN_ID']}"
+workflow_url = session.get(this_run_url).json()["workflow_url"]
 
-#workflow_output = hammock(workflow_url).GET(auth=("github-actions", token))
-#workflow_id = json.loads(workflow_output.text or workflow_url.content)["id"]
+runs = session.get(f"{workflow_url}/runs").json()
+run_index = 0
 
-runs = hammock(workflow_url, auth=("github-actions", token)).runs.GET().json()
-run_index = None
-for run in runs["workflow_runs"]:
+for i, run in enumerate(runs["workflow_runs"]):
     if str(run["id"]) == str(os.environ["GITHUB_RUN_ID"]):
-        run_index = runs["workflow_runs"].index(run)
-for run in runs["workflow_runs"]:
-    if runs["workflow_runs"].index(run) > run_index and str(run["id"]) != str(os.environ["GITHUB_RUN_ID"]) and run["status"] != "completed":
+        run_index = i
+        break
+
+for i, run in enumerate(runs["workflow_runs"]):
+    if i > run_index and str(run["id"]) != str(os.environ["GITHUB_RUN_ID"]) and run["status"] != "completed":
         print(f"Another build ({run['id']} with status {run['status']}) is running, cancelling this one...")
-        cancel_request = this_run.cancel.POST()
+        cancel_request = session.post(f"{this_run_url}/cancel")
         if cancel_request.status_code != 202:
-            sys.exit("Status code did not match: " + cancel_request.status_code)
+            sys.exit(f"Status code did not match: {cancel_request.status_code}")
         else:
             print("Cancel request acknowledged, sleeping 10 seconds to account for delay...")
             time.sleep(10)
