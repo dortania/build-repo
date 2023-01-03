cfe5e1da8a9833241997fc141cb7ef46b64546f0
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2023-01-03 17:10:03 -0500

    Put plugin-specific in a folder

--- config_mgmt.py
+++ config_mgmt.py
@@ -4,12 +4,14 @@ from pathlib import Path
 
 def save_config(data: dict):
     config_dir = Path(__file__).parent.absolute() / Path("Config")
+    plugin_dir = config_dir / Path("plugins")
+    plugin_dir.mkdir(exist_ok=True)
 
     for plugin in data:
         if plugin == "_version":
             continue
         data[plugin]["versions"].sort(key=lambda x: (x["date_committed"], x["date_authored"]), reverse=True)
-        json.dump(data[plugin] | {"_version": data["_version"]}, (config_dir / Path(f"{plugin}.json")).open("w"), sort_keys=True)
+        json.dump(data[plugin] | {"_version": data["_version"]}, (plugin_dir / Path(f"{plugin}.json")).open("w"), sort_keys=True)
 
     latest = data.copy()
     for plugin in latest:
